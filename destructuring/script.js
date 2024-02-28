a67a6ff8410c49d84e417a21e5ed8c254ed171fe
Arvel Kartawijaya <arvel.kartawijaya@binus.ac.id>
2024-02-28 22:43:29 +0700

    destructuring

---
+++ destructuring/script.js
@@ -0,0 +1,18 @@
+//array
+// const nama = ['arvel', 'arvel2', 'arvel3']
+
+// const[nama1, nama2, nama3] = nama;
+// console.log(nama1);
+// console.log(nama2);
+// console.log(nama3);
+
+//object
+const mhs = {
+  nama: 'arvel',
+  nim: 2540131071,
+  email: 'arvel.kartawijaya@binus.ac.id'
+}
+const {nama, nim, email} = mhs;
+console.log(nama);
+console.log(nim);
+console.log(email);
